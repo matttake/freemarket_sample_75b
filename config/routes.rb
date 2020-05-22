34103acc1cb98f1e8be2f9ab0e7c782c74ade7d3
yuuki920 <yuuki.k.1672@gmail.com>
2020-05-22 16:27:12 +0900

    setting_view_toppage

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root to: "tops#index"
+  resources :tops do
+    collection do
+      get 'search'
+    end
+  end
 end
