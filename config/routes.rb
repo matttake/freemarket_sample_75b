33194bc393cc38a698642e5a572c15fec234e14c
yuuki920 <yuuki.k.1672@gmail.com>
2020-05-24 10:52:47 +0900

    setting_view_item_confimation

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
   # トップページ
-  root to: "tops#index"
+  root to: "items#confimation"
 
   # マージしてから修正するので一旦コメントアウト
   # root to: 'view#edit'
   # root 'items#index'
-end
+  
+
