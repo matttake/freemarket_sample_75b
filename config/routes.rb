2036cc0dd59bb05bfe8a26372d69388c17cc011a
nukkey <surouraihu15@gmail.com>
2020-05-24 12:54:33 +0900

    fix

--- config/routes.rb
+++ config/routes.rb
@@ -1,19 +1,27 @@
 Rails.application.routes.draw do
-  
+
   root to: 'itemdeta#index' 
-  root to: 'view#edit'
-  root 'items#index'
+  
+  # 一旦、自分(貫山)の作ったビュー、ファイル最下部にまとめました
+#   root to: 'view#edit'
+#   root 'items#index'
+# end
+
+#   root to: "tops#index"
+
+#   resources :users, only: [:show] do
+#     マイページ表示用の仮アクション
+#     collection do
+#       get 'logout'
+#       get 'card_index'
+#       get 'card_register'
+#     end
+#   end
+
 end
 
-  root to: "tops#index"
 
-  resources :users, only: [:show] do
-    # マイページ表示用の仮アクション
-    collection do
-      get 'logout'
-      get 'card_index'
-      get 'card_register'
-    end
-  end
-
-end
+# itemdeta#index --- 商品詳細ページ
+# view#index --- 新規登録画面
+# view#new --- ログイン画面
+# view#edit --- 登録完了画面
