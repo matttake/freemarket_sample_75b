31272051cb758076c8aa64a6b702ccf9f5ee4466
nukkey <surouraihu15@gmail.com>
2020-05-24 13:01:19 +0900

    fix

--- config/routes.rb
+++ config/routes.rb
@@ -9,14 +9,14 @@ Rails.application.routes.draw do
 
 #   root to: "tops#index"
 
-#   resources :users, only: [:show] do
-#     マイページ表示用の仮アクション
-#     collection do
-#       get 'logout'
-#       get 'card_index'
-#       get 'card_register'
-#     end
-#   end
+  resources :users, only: [:show] do
+    マイページ表示用の仮アクション
+    collection do
+      get 'logout'
+      get 'card_index'
+      get 'card_register'
+    end
+  end
 
 end
 
