de5d612369bef7475460282cfa96e177671c7607
Yuto Tsukamoto <t.fr.swim@icloud.com>
2020-05-26 17:08:21 +0900

    ActiveHash実装

--- app/models/item.rb
+++ app/models/item.rb
@@ -3,4 +3,6 @@ class Item < ApplicationRecord
   belongs_to :category
   has_many :images
   accepts_nested_attributes_for :images, allow_destroy: true
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to_active_hash :prefecture
 end
