e154963fbed92f851ef2409bae0fe2147638a138
Yuto Tsukamoto <t.fr.swim@icloud.com>
2020-05-26 17:28:09 +0900

    商品出品form設定

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -16,17 +16,19 @@ class ItemsController < ApplicationController
 
   def create
     @items = Item.new(item_params)
+    # 現在はuser_idが無いから保存できない
     if @items.save
       redirect_to root_path
     else
-      render :new
+      render :exhibition
     end
   end
   
   private
   
  def item_params
-    params.reqire(:item).permit(:category_id, :name, :description, :stats, :delivery_charge, :delivery_origin_area, :days_until_delivery, :price, images_attributes: [:url]).merge(user_id: current_user.id)
+    params.require(:item).permit(:category_id, :name, :description, :stats, :delivery_charge, :delivery_origin_area, :days_until_delivery, :price, images_attributes: [:url])
+    #ログイン機能実装後付け加える→ .merge(user_id: current_user.id)
   end
   
 end
