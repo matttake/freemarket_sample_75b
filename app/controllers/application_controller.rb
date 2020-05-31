fa8d06b4129807e0d9b4443922446ec2c079e2a1
nukkey <surouraihu15@gmail.com>
2020-06-01 00:42:42 +0900

    マイページへのリンク

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,7 @@ class ApplicationController < ActionController::Base
   before_action :basic_auth, if: :production?
   before_action :configure_permitted_parameters, if: :devise_controller?
   before_action :set_category
+  before_action :set_current_user
 
   private
   def basic_auth
@@ -27,5 +28,9 @@ class ApplicationController < ActionController::Base
   # カテゴリ情報の取得(親要素のみ)
   def set_category
     @parents = Category.where(ancestry: nil)
+  end
+  # ログイン中のユーザを取得
+  def set_current_user
+    @current_user = User.find_by(id: session[:user_id])
   end
 end
