797d3d3807c8db46da29bc9a9f877b6d32926241
nukkey <surouraihu15@gmail.com>
2020-05-31 20:16:50 +0900

    マイグレーションファイル名前変更

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_29_035655) do
+ActiveRecord::Schema.define(version: 2020_05_29_072938) do
 
   create_table "categories", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
@@ -41,11 +41,6 @@ ActiveRecord::Schema.define(version: 2020_05_29_035655) do
     t.datetime "updated_at", null: false
     t.integer "buyer_id"
     t.integer "saler_id", null: false
-  end
-
-  create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
   end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
