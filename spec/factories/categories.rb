e8f41733b1aeda05f6abd3774ac501d8c69c6808
Yuto Tsukamoto <t.fr.swim@icloud.com>
2020-06-04 11:08:21 +0900

    不要コメントアウト削除

--- spec/factories/categories.rb
+++ spec/factories/categories.rb
@@ -2,6 +2,5 @@ FactoryBot.define do
 
   factory :category do
     name   {"親カテゴリ"}
-    # item
   end
 end
--- spec/factories/images.rb
+++ spec/factories/images.rb
@@ -2,7 +2,6 @@ FactoryBot.define do
 
   factory :image do
     
-    # url   {File.open("#{Rails.root}/spec/factories/image.jpg")}
     url {Rack::Test::UploadedFile.new(File.join(Rails.root, 'spec/factories/image.jpg'))}
     item
   end
