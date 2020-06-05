bc242b24c759ef94aa91ad2a36d145422fefa889
yuuki920 <yuuki.k.1672@gmail.com>
2020-06-05 18:34:03 +0900

    payjpの環境変数設定

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -21,11 +21,12 @@ class ItemsController < ApplicationController
     end
   end
 
+  #商品購入確定画面
   def confimation
     @payment = Payment.find_by(user_id: current_user.id)
     if @payment.blank?
     else
-      Payjp.api_key = ENV["PAYJP_PRIVATE_KEY"]
+      Payjp.api_key = Rails.application.credentials.payjp[:PAYJP_PRIVATE_KEY]
       customer = Payjp::Customer.retrieve(@payment.customer_id)
       @default_card_information = customer.cards.retrieve(@payment.card_id)
       @exp_month =@default_card_information.exp_month.to_s
